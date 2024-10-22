77615ecbc3d494808440eea5cdd6bd58d943a5bf
Pavan-2628 <pavanrasal347@gmail.com>
2024-10-22 22:33:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,3 +4,31 @@ resource "github_repository" "git_repo_terraform" {
 
     visibility = "public"
 } 
+
+
+
+
+
+
+
+# main.tf
+
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "my-access-key"
+  secret_key = "my-secret-key"
+}
+# Create an S3 bucket
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "my-terraform-s3-bucket-name"
+  acl    = "private"                
+
+  tags = {
+    Name        = "My S3 Bucket"
+    Environment = "Dev"
+  }
+}
+
+
+
+
